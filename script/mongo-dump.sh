1c79bee23b299fc1204563212c10c053fd5554a6
notoday <no.today@outlook.com>
2021-01-27 16:22:42 +0800

    Mongodb backup

--- script/mongo-dump.sh
+++ script/mongo-dump.sh
@@ -10,5 +10,13 @@ PASSWORD=$3
 # 宿主机备份目录
 BACKUP_PATH=$4
 
-docker exec $CONTAINER_NAME mongodump -h 127.0.0.1 --port 27017 -u=$USERNAME -p=$PASSWORD -o /dump
-docker cp $CONTAINER_NAME:/dump $BACKUP_PATH
+# 备份时间标记
+DATETIME=$(date +%Y-%m-%d_%H:%M:%S)
+
+echo " 开始备份: $CONTAINER_NAME $(date +%Y-%m-%d_%H:%M:%S)"
+
+docker exec $CONTAINER_NAME mongodump --host 127.0.0.1 --port 27017 -u $USERNAME -p $PASSWORD --authenticationDatabase admin -o /dump/$DATETIME
+docker cp $CONTAINER_NAME:/dump/$DATETIME $BACKUP_PATH/$DATETIME
+docker exec $CONTAINER_NAME rm -rf /dump/$DATETIME
+
+echo " 执行完成: $CONTAINER_NAME $(date +%Y-%m-%d_%H:%M:%S)"
--- script/mongo-restore.sh
+++ script/mongo-restore.sh
@@ -10,5 +10,10 @@ PASSWORD=$3
 # 宿主机备份目录
 BACKUP_PATH=$4
 
+echo " 开始恢复: $CONTAINER_NAME $(date +%Y-%m-%d_%H:%M:%S)"
+
 docker cp $BACKUP_PATH $CONTAINER_NAME:/restore
-docker exec $CONTAINER_NAME -h 127.0.0.1 --port 27017 -u=$USERNAME -p=$PASSWORD /restore
+docker exec $CONTAINER_NAME mongorestore --host 127.0.0.1 --port 27017 -u $USERNAME -p=$PASSWORD --authenticationDatabase admin /restore
+docker exec $CONTAINER_NAME rm -rf /restore
+
+echo " 执行完成: $CONTAINER_NAME $(date +%Y-%m-%d_%H:%M:%S)"
